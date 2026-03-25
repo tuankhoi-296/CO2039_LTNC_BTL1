eaa49170ca15d728ab3c6ef4ce7a62036c6fd180
tuankhoi-296 <tuankhoile020906@gmail.com>
2026-03-25 22:22:48 +0700

    3.2

--- Bond.java
+++ Bond.java
@@ -4,34 +4,33 @@ public class Bond extends Instrument {
 
     public Bond(String symbol, String name, double currentPrice, double couponRate, int maturityYears) {
         super(symbol, name, currentPrice);
-        // TODO
-        throw new UnsupportedOperationException("TODO");
+        this.couponRate = couponRate;
+        this.maturityYears = maturityYears;
     }
 
     @Override
     public double riskScore() {
-        // TODO
-        throw new UnsupportedOperationException("TODO");
+        if (this.maturityYears > 10) {
+            return 4.0;
+        } else {
+            return 2.0;
+        }
     }
 
     @Override
     public String assetClass() {
-        // TODO
-        throw new UnsupportedOperationException("TODO");
+        return "FIXED_INCOME";
     }
 
     public double annualCouponPayment(int units) {
-        // TODO
-        throw new UnsupportedOperationException("TODO");
+        return units * this.getCurrentPriceValue() * this.couponRate / 100.0;
     }
 
     public double getCouponRate() {
-        // TODO
-        throw new UnsupportedOperationException("TODO");
+        return this.couponRate;
     }
 
     public int getMaturityYears() {
-        // TODO
-        throw new UnsupportedOperationException("TODO");
+        return this.maturityYears;
     }
 }
